a4304c4f7c25eeb5fd42639a1a4a95ce08d00945
hpompougnac <hpompougnac@pc-164.home>
2024-07-17 17:43:53 +0200

    Update + Notes

--- src/sail_xdsl_backend/MlirDef.ml
+++ src/sail_xdsl_backend/MlirDef.ml
@@ -2,16 +2,23 @@ type id = string
 
 type mlir_builtin_scalar_types = I2 | I4 | I8 | I16 | I32 | I64
 
+type dim = Question | Size of int
+
+type general_dims = Poly | Dims of dim list
+
 type mlir_builtin_multidim_types =
-  Tensor of int list * mlir_builtin_scalar_types
-| MemRef of int list * mlir_builtin_scalar_types
+  Tensor of general_dims * mlir_builtin_scalar_types
+| MemRef of general_dims * mlir_builtin_scalar_types
 
 type mlir_builtin_types =
   Scalar of mlir_builtin_scalar_types
 | MultiDim of mlir_builtin_multidim_types
 
+type num = Lit of int | FreeVar of id | Var of id
+
 type mlir_type =
   Builtin of mlir_builtin_types
+| Num of num
 | Typ_id of id
 | Typ_parameterized of id * mlir_type list 
 | NotImplemented of string
@@ -41,10 +48,21 @@ let string_of_mlir_builtin_scalar_type t = match t with
 | I32 -> "i32"
 | I64 -> "i64"
 
+let string_of_dim d = match d with
+    Question -> "?"
+  | Size i -> string_of_int i
+
+let string_of_general_dims ds = match ds with
+    Poly -> "*"
+  | Dims [] -> assert(false)
+  | Dims(h::t) ->
+     string_of_dim h ^ List.fold_left (fun acc e -> acc ^ "x" ^ string_of_dim e) "" t
+
 let string_of_mlir_builtin_multidim_type t = match t with
     Tensor(il,t) ->
      "tensor<"
-     ^ List.fold_left (fun acc e -> acc ^ string_of_int e ^ "x") "" il
+     ^ string_of_general_dims il
+     ^ "x"
      ^ string_of_mlir_builtin_scalar_type t
      ^ ">"
   | MemRef(il,t) -> assert(false)
@@ -53,8 +71,13 @@ let string_of_mlir_builtin_type t = match t with
     Scalar t -> string_of_mlir_builtin_scalar_type t
   | MultiDim t -> string_of_mlir_builtin_multidim_type t
 
+let string_of_num n = match n with
+    Lit l -> string_of_int l
+  | FreeVar v | Var v -> v
+
 let rec string_of_mlir_type t = match t with
     Builtin t' -> string_of_mlir_builtin_type t'
+  | Num i -> string_of_num i
   | Typ_id id -> id
   | Typ_parameterized(id, params_list) ->
      let string_params_list = match List.map string_of_mlir_type params_list with
@@ -65,7 +88,9 @@ let rec string_of_mlir_type t = match t with
 
 let string_of_mlir_def mlir_def = match mlir_def with
     Nop -> ""
-  | NotImplemented e -> "NotImplemented(" ^ e ^ ")\n"
+  | NotImplemented e ->
+     ""
+     (* "NotImplemented(" ^ e ^ ")\n" *)
   | TypeDef(id,e) -> "type " ^ id ^ ": " ^ string_of_mlir_type(e) ^ "\n"
   | RegDef(id,mlir_type) ->
      "register " ^ id ^ ": " ^ string_of_mlir_type mlir_type ^ "\n"
--- src/sail_xdsl_backend/xdsl_backend.ml
+++ src/sail_xdsl_backend/xdsl_backend.ml
@@ -20,18 +20,9 @@ let string_of_id id = match id with
   | Id_aux(Operator(_),_) -> assert(false)
 
 let process_nexp_aux (nexp_aux: nexp_aux): mlir_type = match nexp_aux with
-    Nexp_id(id) -> NotImplemented("Nexp_id")
-  | Nexp_var(Kid_aux(Var(v),_)) -> NotImplemented("Nexp_var")
-  | Nexp_constant(num) ->
-     let mlir_type = match Nat_big_num.to_int(num) with
-         2 -> Builtin(Scalar(I2))
-       | 4 -> Builtin(Scalar(I4))
-       | 8 -> Builtin(Scalar(I8))
-       | 16 -> Builtin(Scalar(I16))
-       | 32 -> Builtin(Scalar(I32))
-       | 64 -> Builtin(Scalar(I64))
-       | n -> Builtin(MultiDim(Tensor([n],I2))) in
-     mlir_type
+    Nexp_id(id) -> Num(Var(string_of_id id))
+  | Nexp_var(Kid_aux(Var(v),_)) -> Num(FreeVar v)
+  | Nexp_constant(num) -> Num(Lit(Nat_big_num.to_int(num)))
   | Nexp_app(id,nexp_list) -> NotImplemented("Nexp_app")
   | Nexp_if(n_constraint,nexp0,nexp1) -> NotImplemented("Nexp_if")
   | Nexp_times(nexp0, nexp1) -> NotImplemented("Nexp_times")
@@ -40,27 +31,27 @@ let process_nexp_aux (nexp_aux: nexp_aux): mlir_type = match nexp_aux with
   | Nexp_exp(nexp) -> NotImplemented("Nexp_exp")
   | Nexp_neg(nexp) -> NotImplemented("Nexp_neg")
 
-let rec process_dec_spec typs_env defs_env (Typ_aux(reg,_)): mlir_type = match reg with
+let rec process_dec_spec typs_env (Typ_aux(reg,_)): mlir_type = match reg with
     Typ_id id -> Typ_id (string_of_id id)
   | Typ_var(kid) -> NotImplemented("Typ_var")
   | Typ_app(id,typ_arg_list) ->
-     let mlir_types_list = List.map (process_typ_arg typs_env defs_env) typ_arg_list in
+     let mlir_types_list = List.map (process_typ_arg typs_env) typ_arg_list in
      Typ_parameterized(string_of_id id, mlir_types_list)
   | Typ_fn(id_in,typ_list) -> NotImplemented("Typ_fn")
   | Typ_bidir(typ1,typ2) -> NotImplemented("Typ_bidir")
   | Typ_tuple(typ_list) -> NotImplemented("Typ_tuple")
   | Typ_exist(kinded_id_list,typ0,typ1) -> NotImplemented("Typ_exist")
   | Typ_internal_unknown -> NotImplemented("Typ_internal_unknown")
-and process_typ_arg typs_env defs_env (A_aux(typ_arg_aux, _)) = match typ_arg_aux with
+and process_typ_arg typs_env (A_aux(typ_arg_aux, _)): mlir_type = match typ_arg_aux with
     A_nexp(Nexp_aux(nexp_aux,_)) -> (* numeric expression, of kind Int *)
      process_nexp_aux nexp_aux
-  | A_typ(t) -> process_dec_spec typs_env defs_env t
+  | A_typ(t) -> process_dec_spec typs_env t
   | A_bool(NC_aux(nca,_)) -> NotImplemented("DEF_type/TD_abbrev/NC_aux")
     
-let process_typ_def_aux typs_env defs_env typ_def_aux = match typ_def_aux with
+let process_typ_def_aux typs_env typ_def_aux = match typ_def_aux with
     TD_abbrev(id,typquant,typ_arg) ->
      let name = string_of_id id in
-     TypeDef(name,process_typ_arg typs_env defs_env typ_arg)
+     TypeDef(name,process_typ_arg typs_env typ_arg)
      (* NotImplemented("DEF_type/TD_abbrev/A_typ") *)
   | TD_record(id,typquant,typ_id_list,bool) ->
      TypeDef(string_of_id id, NotImplemented("DEF_type/TD_record"))
@@ -73,11 +64,11 @@ let process_typ_def_aux typs_env defs_env typ_def_aux = match typ_def_aux with
   | TD_bitfield(id,typ, id_index_range_list) ->
      TypeDef(string_of_id id, NotImplemented("DEF_type/TD_bitfield"))
 
-let process_def_aux typs_env defs_env (DEF_aux(aux,_)) = match aux with
-    DEF_type(TD_aux(typ_def_aux,_)) -> process_typ_def_aux typs_env defs_env typ_def_aux
+let process_def_aux typs_env (DEF_aux(aux,_)) = match aux with
+    DEF_type(TD_aux(typ_def_aux,_)) -> process_typ_def_aux typs_env typ_def_aux
   | DEF_register(DEC_aux(DEC_reg(reg,id,_),_)) -> (* register declaration *)
      let name = string_of_id id in
-     RegDef(name, process_dec_spec typs_env defs_env reg)
+     RegDef(name, process_dec_spec typs_env reg)
   | DEF_constraint(atyp) -> NotImplemented("DEF_constraint")
   | DEF_fundef(fundef) -> NotImplemented("DEF_fundef")
   | DEF_mapdef(mapdef) -> NotImplemented("DEF_mapdef") (* mapping definition *)
@@ -98,8 +89,6 @@ let process_def_aux typs_env defs_env (DEF_aux(aux,_)) = match aux with
 (* Env: ~/src/upstream/sail/src/lib/type_env.ml *)
 (* Ast: ./src/lib/type_check.mli:type typed_ast = (tannot, env) ast *)
 let process_ast (typs_env: Env.t) (o: out_channel) ({defs = d; comments = _}: typed_ast) =
-  let defs_env = { types_aliases = ref []} in
-  let mlir_defs = List.map (process_def_aux typs_env defs_env) d in
+  let mlir_defs = List.map (process_def_aux typs_env) d in
   let strings_mlir_defs = List.map string_of_mlir_def mlir_defs in
-  string_of_defs_env defs_env |> print_string;
   List.iter print_string strings_mlir_defs;
